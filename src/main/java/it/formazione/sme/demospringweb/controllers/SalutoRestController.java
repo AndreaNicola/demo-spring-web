d3da42d8f7227913fb85879ea466b18839b1ded5
Andrea Nicola <anicola@key-biz.it>
2019-05-17 17:07:40 +0200

    Edit: cambio endpoint

--- src/main/java/it/formazione/sme/demospringweb/controllers/SalutoRestController.java
+++ src/main/java/it/formazione/sme/demospringweb/controllers/SalutoRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
-@RequestMapping("saluti")
+@RequestMapping("rest/saluti")
 public class SalutoRestController {
 
   private final ISalutoService salutoService;
